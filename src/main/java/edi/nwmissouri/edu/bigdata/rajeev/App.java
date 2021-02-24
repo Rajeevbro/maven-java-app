7cde1ec1ffef279ef1e905315a72c10e3a41c8a6
Rajeev Chapagain <31379467+Rajeevbro@users.noreply.github.com>
2021-02-24 16:35:58 -0600

    pushing files

---
+++ src/main/java/edi/nwmissouri/edu/bigdata/rajeev/App.java
@@ -0,0 +1,13 @@
+package edi.nwmissouri.edu.bigdata.rajeev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Rajeev!" );
+    }
+}
